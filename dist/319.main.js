feccfced17ca0ed5622dbced237d20d22aac9146
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-27 21:03:45 +0100

    delete dist

--- dist/319.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktemplate=self.webpackChunktemplate||[]).push([[319],{319:(e,t,n)=>{function u(){return"\n    <h1>Checkout</h1>\n    "}n.r(t),n.d(t,{render:()=>u})}}]);
