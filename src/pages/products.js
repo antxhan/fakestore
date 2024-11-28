34c7315345b2474f47643a920a0b32e239bc6d2d
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-28 13:45:44 +0100

    feat: async component loading + basic products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,15 +1,37 @@
 import { api } from "../utils/api";
 
-export async function render() {
-  const products = await api.products(3);
-  const product = await api.product(3);
-  console.log(products);
-  console.log("product", product);
+const PRODUCTS_PER_PAGE = 20;
+
+function createHTML(productList) {
   return `
-        <h1>Products</h1>
-        <ul>
-        ${products
-          .map((product) => `<li>${product.title}</li>`)
-          .join("")}           
-        </ul>`;
+    <h1>Products</h1>
+    <ul>${productList}</ul>
+    `;
+}
+
+export function render(callback) {
+  // create a skeleton HTML to show while products are loading
+  const skeletonHTML = createHTML("Loading...");
+
+  // asynchronously getting products, then updating the DOM
+  api
+    .products(PRODUCTS_PER_PAGE)
+    .then((products) => {
+      const productList = products
+        .map((product) => `<li>${product.title}</li>`)
+        .join("");
+
+      // callback to update the DOM
+      const html = createHTML(productList);
+      if (callback) callback(html);
+    })
+    .catch((error) => {
+      console.error("Error fetching products:", error);
+
+      // callback to update the DOM
+      const html = createHTML("Failed to load products");
+      if (callback) callback(html);
+    });
+
+  return skeletonHTML;
 }
