78c413510a7fa224330c1c4bf7692fdde64e8099
yusun-lab <yu.sun@hyperisland.se>
2024-11-29 15:17:53 +0100

    Get ready to merge main

--- src/likeButton.js
+++ src/likeButton.js
@@ -3,6 +3,5 @@ document.querySelectorAll('.like-btn').forEach(button => {
   button.addEventListener('click', () => {
     // Toggle the "liked" class to change the style
     button.classList.toggle('liked');
-    // button.textContent = button.classList.contains('liked') ? '♥' : '♡';
   });
 });
