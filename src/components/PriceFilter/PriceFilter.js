c2b8a80af16993d479117aca322eea970ba1d1ba
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2024-12-03 13:13:46 +0100

    Almost done with price filter

--- src/components/PriceFilter/PriceFilter.js
+++ src/components/PriceFilter/PriceFilter.js
@@ -2,6 +2,40 @@ import "./PriceFilter.css";
 
 export default function PriceFilter() {
   return `
-  <div class="price-filter">PriceFilter YOU SEE THIS</div>
+  <button id="toggleButton">Toggle Price Filter</button>
+    <div class="dropdown">
+      <div class="priceFilterContainer">
+        <div class="price-input">
+          <div class="field">
+            <span>Min</span>
+            <input type="number" class="input_min" value="2500" />
+          </div>
+          <div class="seperator">-</div>
+          <div class="field">
+            <span>Max</span>
+            <input type="number" class="input_max" value="7500" />
+          </div>
+        </div>
+        <div class="slider">
+          <div class="progress"></div>
+        </div>
+        <div class="range-input">
+          <input
+            type="range"
+            class="range-min"
+            min="0"
+            max="1000"
+            value="2500"
+          />
+          <input
+            type="range"
+            class="range-max"
+            min="0"
+            max="1000"
+            value="7500"
+          />
+        </div>
+      </div>
+    </div>  
   `;
 }
