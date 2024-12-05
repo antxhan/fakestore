97dc83951ed5d48247a7f28dc0ff790f499afcb0
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-12-05 15:23:50 +0100

    refactor(style): product filters and product page + feat: template for handling category filter

--- src/components/CategoriesFilter/CategoriesFilter.js
+++ src/components/CategoriesFilter/CategoriesFilter.js
@@ -4,10 +4,10 @@ export default function CategoriesFilter() {
   return `<div class="categories-filter">
             <p class="filter-title">Categories</p>
             <div class="filter-options">
-              <button class="filter-btn">Electronics</button>
-              <button class="filter-btn">Jewelery</button>
-              <button class="filter-btn">Men's Clothing</button>
-              <button class="filter-btn">Women's Clothing</button>
+              <button class="filter-btn" value="electronics">Electronics</button>
+              <button class="filter-btn" value="jewelery">Jewelery</button>
+              <button class="filter-btn" value="men's clothing">Men's Clothing</button>
+              <button class="filter-btn" value="women's clothing">Women's Clothing</button>
             </div>
           </div>
   `;
