b61e3e9af98f70731a17d103c734d40b19b74115
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-12-05 11:39:24 +0100

    feat: product page loading correct product + quantity button event listeners

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,54 +1,132 @@
 import "./product.css";
-import productImage from "./Images/coat.png";
-export function render() {
+import { api } from "../../utils/api";
+import { db } from "../../utils/db";
+import heartOutline from "../../assets/icons/heart-outline.svg";
+import heartFilled from "../../assets/icons/heart-filled.svg";
+
+function createHTML(product) {
+  const loadingMessage = "Loading...";
   return `
-    <div class="product-container">
-        <div class="product-image">
-           <img src="${productImage}" alt="Faux Leather Coat">
-        </div>
-        <div class="product-info">
-            <div class="product-details">
-                <div class="product-name">
-                    <h3>Topshop Faux Leather Coat</h3>
-                </div>
-                <div class="product-price">
-                    <p>Now 979.00 SEK</p>
-                </div>
-                <div class="product-description">
-                    <p>Black coat in faux leather with matte finish. Oversized fit.</p>
-                </div>
-                <hr>
-                <div class="product-selection">
-                    <div class="size-selection">
-                        <p class="selection-title">Size: <strong>M</strong></p>
-                        <div class="size-options">
-                            <button class="size-button">XS</button>
-                            <button class="size-button">S</button>
-                            <button class="size-button">M</button>
-                            <button class="size-button">L</button>
-                            <button class="size-button selected">XL</button>
-                        </div>
-                    </div>
-                    <div class="quantity-selection">
-                        <p class="selection-title">Qty</p>
-                        <div class="quantity-controls">
-                            <button class="qty-button" id="decrease">−</button>
-                            <span class="qty-number">1</span>
-                            <button class="qty-button" id="increase">+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="add-to-bag-container">
-                    <button class="add-to-bag-button">Add to cart</button>
-                    <div class="heart-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                        </svg>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  `;
+  <div class="product-container container">
+      <div class="product-image">
+         <img src="${product.image || loadingMessage}" alt="${
+    product.title || loadingMessage
+  }">
+      </div>
+      <div class="product-info">
+          <div class="product-details">
+              <div class="product-name">
+                  <h3>${product.title || loadingMessage}</h3>
+              </div>
+              <div class="product-price">
+                  <p>$${product.price || loadingMessage}</p>
+              </div>
+              <div class="product-description">
+                  <p>${product.description || loadingMessage}</p>
+              </div>
+              <hr>
+              <div class="product-selection">
+                  <div class="size-selection">
+                      <p class="selection-title">Size: <strong>M</strong></p>
+                      <div class="size-options">
+                          <button class="size-button">XS</button>
+                          <button class="size-button">S</button>
+                          <button class="size-button">M</button>
+                          <button class="size-button">L</button>
+                          <button class="size-button selected">XL</button>
+                      </div>
+                  </div>
+                  <div class="quantity-selection">
+                      <p class="selection-title">Qty</p>
+                      <div class="quantity-controls">
+                          <button class="qty-button" id="decrease" value="-">−</button>
+                          <span class="qty-number">1</span>
+                          <button class="qty-button" id="increase" value="+">+</button>
+                      </div>
+                  </div>
+              </div>
+              <div class="add-to-bag-container">
+                  <button class="add-to-bag-button">Add to cart</button>
+                  <div class="heart-icon">
+                      <img src="${
+                        product.liked ? heartFilled : heartOutline
+                      }" alt="${
+    product.liked ? "Unlike" : "Like"
+  }" class="heart-icon">
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+`;
 }
 
+function handleQuantityButtons() {
+  const quanitityButtons = document.querySelectorAll(
+    ".quantity-controls button"
+  );
+  quanitityButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const quantityContainer = button.parentNode.querySelector("span");
+      const quantity = parseInt(quantityContainer.innerHTML);
+      if (e.currentTarget.value === "-") {
+        if (quantity === 1) return;
+        quantityContainer.innerHTML = quantity - 1;
+      } else {
+        quantityContainer.innerHTML = quantity + 1;
+      }
+    });
+  });
+}
+
+function handleAddToCart(productId) {
+  // cart is set up in local storage and looks like this, for example:
+  // {
+  //   1: 3,
+  //   3: 5,
+  // }
+  // the key is the productId and the value is the quantity
+}
+
+function handleLike(productId) {
+  // likes is set up in local storage and looks like this, for example:
+  // [1, 3, 5]
+  // the value is the productId
+}
+
+export async function render(callback) {
+  // create a skeleton HTML to show while product is loading
+  const skeletonHTML = createHTML("Loading...");
+
+  // getting product id from url
+  const url = new URL(window.location.href);
+  const searchParams = new URLSearchParams(url.search);
+  const productId = searchParams.get("id");
+
+  // asynchronously getting product, then updating the DOM
+  api
+    .product(productId)
+    .then((product) => {
+      // checks if has been liked
+      const likedProducts = db.getLikes();
+      if (likedProducts.includes(productId)) {
+        product.liked = true;
+      }
+
+      // callback to update the DOM
+      const html = createHTML(product);
+      if (callback) callback(html);
+
+      // add event Listeners
+      handleQuantityButtons();
+      handleAddToCart(productId);
+      handleLike(productId);
+    })
+    .catch((error) => {
+      console.error("Error fetching product:", error);
+    });
+
+  return skeletonHTML;
+}
