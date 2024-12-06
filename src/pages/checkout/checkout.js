d565796cec45ed6143215a9fe6a3c75043c1ceb7
hannac7 <yourname@example.com>
2024-12-06 15:12:10 +0100

    checkout container

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,11 +1,96 @@
 import "./checkout.css";
 import Breadcrumbs from "../../components/Breadcrumbs";
 
-export function render() {
+function createHTML() {
   return `
   ${Breadcrumbs("Checkout")}
-    <h1>Checkout</h1>
-    <div class="checkout-container">
-
+    <div class="checkout-container container">
+      <h2>Delivery and Payment</h2>
+        <form id="delivery-form">
+          <div class="form-group">
+                <label for="first-name">First Name</label>
+                <input type="text" id="first-name" name="first-name" required>
+            </div>
+            <div class="form-group">
+                <label for="last-name">Last Name</label>
+                <input type="text" id="last-name" name="last-name" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email Address</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="postal-code">Postal Code</label>
+                <input type="text" id="postal-code" name="postal-code" required>
+            </div>
+            <div class="form-group">
+                <label for="personal-number">Personal Number (optional)</label>
+                <input type="text" id="personal-number" name="personal-number">
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <input type="text" id="address" name="address" required>
+            </div>
+            <div class="form-group">
+                <label for="city">City</label>
+                <input type="text" id="city" name="city" required>
+            </div>
+            <div class="form-group">
+                <label for="mobile-number">Mobile Number</label>
+                <input type="tel" id="mobile-number" name="mobile-number" required>
+            </div>
+            <hr>
+            <div class="form-group">
+                <label for="credit-card-number">Credit Card Number</label>
+                <input type="text" id="credit-card-number" name="credit-card-number" required>
+            </div>
+            <div class="form-group">
+                <label for="security-code">Security Code</label>
+                <input type="text" id="security-code" name="security-code" required>
+            </div>
+             <div class="name-on-card">
+                <label for="sname-on-card">Name on Card</label>
+                <input type="text" id="name-on-card" name="name-on-card" required>
+            </div>
+            <div class="form-actions">
+                <button type="button" id="cancel">Cancel</button>
+                <button type="submit" id="continue">Continue</button>
+            </div>
+        </form>
     `;
 }
+
+export function render(callback) {
+  const html = createHTML()
+
+  setTimeout(() => {
+
+  // callback to update the DOM
+  if (callback) callback(html);
+
+
+  const cancelButton = document.getElementById('cancel')
+  console.log(cancelButton)
+  const deliveryForm = document.querySelector('#delivery-form')
+  console.log(deliveryForm)
+  cancelButton.addEventListener('click', () => {
+   console.log("click")
+   deliveryForm.reset();
+ });
+
+ 
+document.getElementById('delivery-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Form submitted!');
+
+    window.location.href = "/"
+
+});
+    
+  }, 1000)
+  
+
+
+  return html
+}
+
